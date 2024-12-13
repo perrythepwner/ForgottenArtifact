c5e18632f9cfb759b8dd0383f81984fc2646ec4e
simonedimaria <117766011+perrythepwner@users.noreply.github.com>
2024-12-13 14:35:33 +0100

    fixed rpc

--- build-docker.sh
+++ build-docker.sh
@@ -8,15 +8,12 @@ PUBLIC_IP=127.0.0.1
 HANDLER_PORT=8000
 LOCAL_RPC_PORT=5000
 LOCAL_RPC_URL=http://localhost:${LOCAL_RPC_PORT}/
-PUBLIC_RPC_PORT=8888
-FRONTEND_PORT=8080
 ################################
 
 docker rm -f $IMAGE
 docker build --tag=$IMAGE:latest ./challenge/ && \
 docker run --rm -it \
-    -p "$PUBLIC_RPC_PORT:$LOCAL_RPC_PORT" \
-    -p "$FRONTEND_PORT:$FRONTEND_PORT" \
+    -p "$LOCAL_RPC_PORT:$LOCAL_RPC_PORT" \
     -p "$HANDLER_PORT:$HANDLER_PORT" \
     --name $IMAGE \
     $IMAGE:latest
